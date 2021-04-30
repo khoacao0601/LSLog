90e31b148d9b1375034da894ae5af993598fc218
khoacao0601 <khoacao0601@gmail.com>
2021-04-30 13:03:06 -0700

    Update Logo and delete comments

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useContext} from 'react';
 import { ControlViewsContext } from '../context/controlViews';
+import LSLLogo from '../images/LSL Logo.png';
 
 const Header = () => {
 
@@ -9,6 +11,7 @@ const Header = () => {
 
     return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <img src={LSLLogo} style={{width:"3%"}}></img>
         <a className="navbar-brand" href="#">LifeScience Logistics</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
